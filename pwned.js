d89bb211a3716d006451d2ca14dcd4e972b55813
Helder Faria <helderjf@gmail.com>
2020-05-02 10:49:45 +0100

    split hash and clean up a bot

--- pwned.js
+++ pwned.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto')
+const http = require('http2')
 
 
 //get password from the command line
@@ -6,8 +7,27 @@ const plainTextPassword = process.argv[2];
 console.log('plain text password = ' + plainTextPassword)
 
 //hash password using sha1 algorithm
-const hashedPassword = crypto
-	.createHash('sha1')
-	.update(plainTextPassword)
-	.digest('hex')
+const hashedPassword = hashPassword(plainTextPassword)
 console.log('hashed password = ' + hashedPassword)
+
+//split hashed password
+const splitedHash = splitHash(hashedPassword)
+console.log('hash head: '+ splitedHash.head)
+console.log('hash tail: '+ splitedHash.tail)
+
+
+
+
+
+function hashPassword(password){
+	return crypto
+		.createHash('sha1')
+		.update(plainTextPassword)
+		.digest('hex')
+}
+
+function splitHash(hash){
+	const head = hash.substr(0,5)
+	const tail  = hash.substr(5)
+	return {head,tail}
+}
