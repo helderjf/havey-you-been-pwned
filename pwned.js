c8766410235a92effc0b7d5bf32fdbebbe7f0a91
Helder Faria <helderjf@gmail.com>
2020-05-02 13:15:10 +0100

    process raw data; check if password has been pwned; inform user

--- pwned.js
+++ pwned.js
@@ -1,38 +1,41 @@
 const crypto = require('crypto')
-const http = require('http2')
 const axios = require('axios')
-
+const chalk = require('chalk')
 
 //get password from the command line
 const plainTextPassword = process.argv[2];
-console.log('plain text password = ' + plainTextPassword)
 
 //hash password using sha1 algorithm
 const hashedPassword = hashPassword(plainTextPassword)
-console.log('hashed password = ' + hashedPassword)
-
 //split hashed password
 const splitedHash = splitHash(hashedPassword)
-console.log('hash head: '+ splitedHash.head)
-console.log('hash tail: '+ splitedHash.tail)
 
+//get pwned hashes from API
 const url = 'https://api.pwnedpasswords.com/range/'
-
 axios.default.get(url + splitedHash.head)
-	.then(response => console.log(response.data))
+	.then(response => checkPwned(response.data))
 	.catch(error => console.log(error))
 
-
-
-
-
-
-
+//check if password has been pwned
+function checkPwned(data){
+	const filteredData = data.split('\r\n')
+	.filter(line =>  line.startsWith(splitedHash.tail))
+	
+	if(filteredData.length != 0){
+		const timesPwned = filteredData[0].split(':')[1]
+		console.log(chalk.white.bgRedBright('!!!Upss!!!!') + '     ' + chalk.redBright('Your password has been pwned ' + timesPwned + ' times!'))
+		console.log(chalk.redBright('You should change it ASAP.'))
+	}else{
+		console.log(chalk.green('Good! Your password hasn\'t been pwned'))
+	}
+}
+	
 function hashPassword(password){
 	return crypto
 		.createHash('sha1')
 		.update(plainTextPassword)
 		.digest('hex')
+		.toUpperCase()
 }
 
 function splitHash(hash){
@@ -40,3 +43,5 @@ function splitHash(hash){
 	const tail  = hash.substr(5)
 	return {head,tail}
 }
+
+
